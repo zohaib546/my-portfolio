63ebf87e7c99a836f8da7acc86ba4812eead203c
zohaib546 <zohaibashraf546@gmail.com>
2025-03-19 04:07:54 +0500

    header file linting error fixed

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,11 @@
 import HeaderSocialList from './header-social-list';
 
 export default function Header() {
+	const name = "I'm Zohaib Ashraf.";
 	return (
 		<header>
 			<p>Hello, World.</p>
-			<h1 className='font-poppins text-3xl'>I'm Zohaib Ashraf.</h1>
+			<h1 className='font-poppins text-3xl'>{name}</h1>
 			<h2 className='font-lora text-3xl'>Front-end Developer UI/UX Designer</h2>
 			<a href='#'>More About Me</a>
 			<HeaderSocialList />
